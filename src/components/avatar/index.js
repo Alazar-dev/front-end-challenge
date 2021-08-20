c78381c644c08856515f7abc7ce7bd124487663b
Alazar-dev <mealazartadesse@gmail.com>
2021-08-20 16:29:40 +0300

    created avatar component

---
+++ src/components/avatar/index.js
@@ -0,0 +1,7 @@
+export default function Avatar() {
+  return (
+    <div>
+      <h1>This is Avatar component</h1>
+    </div>
+  );
+}
---
+++ src/pages/Avatar.js
@@ -0,0 +1,11 @@
+import AvatarComponent from "../components/avatar";
+
+function Avatar() {
+  return (
+    <div>
+      <AvatarComponent />
+    </div>
+  );
+}
+
+export default Avatar;
