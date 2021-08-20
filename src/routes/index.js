44df5d77afe6137f49540f421b8d4a7267961570
Alazar-dev <mealazartadesse@gmail.com>
2021-08-20 16:28:58 +0300

    added page routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,19 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Welcome from "../pages/Welcome";
+import NameAuth from "../pages/NameAuth";
+import EmailAuth from "../pages/EmailAuth";
+import UserPreference from "../pages/UserPreference";
+import Avatar from "../pages/Avatar";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path to="/" component={Welcome} />
+        <Route exact path="/" component={Welcome} />
+        <Route exact path="/auth" component={NameAuth} />
+        <Route exact path="/emailAuth" component={EmailAuth} />
+        <Route exact path="/userpreference" component={UserPreference} />
+        <Route exact path="/avatar" component={Avatar} />
       </Switch>
     </BrowserRouter>
   );
