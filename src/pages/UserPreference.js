f34ed9a0d274b411c5d38e7e5e402cd0eb60a843
Alazar-dev <mealazartadesse@gmail.com>
2021-08-20 16:26:46 +0300

    added theme and toggler

---
+++ src/pages/UserPreference.js
@@ -0,0 +1,12 @@
+import React from "react";
+import UserPreferenceComponent from "../components/userPreference";
+
+function UserPreference() {
+  return (
+    <div>
+      <UserPreferenceComponent />
+    </div>
+  );
+}
+
+export default UserPreference;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "class", // or 'media' or 'class'
+  experimental: {
+    darkModeVariant: true,
+  },
   theme: {
     extend: {},
   },
@@ -8,4 +11,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
