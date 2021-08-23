0eb5286ae22b87db8609f89d537c03c7dd9e9052
Alazar-dev <mealazartadesse@gmail.com>
2021-08-23 16:11:23 +0300

    added components to router

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ import NameAuth from "../pages/NameAuth";
 import EmailAuth from "../pages/EmailAuth";
 import UserPreference from "../pages/UserPreference";
 import Avatar from "../pages/Avatar";
+import Success from "../pages/Success";
+import StartOver from "../pages/StartOver";
 
 export default function Routes() {
   return (
@@ -14,6 +16,8 @@ export default function Routes() {
         <Route exact path="/emailAuth" component={EmailAuth} />
         <Route exact path="/userpreference" component={UserPreference} />
         <Route exact path="/avatar" component={Avatar} />
+        <Route exact path="/success" component={Success} />
+        <Route exact path="/startover" component={StartOver} />
       </Switch>
     </BrowserRouter>
   );
