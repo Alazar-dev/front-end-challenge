7ad21e4672a88656843e806e562eae9f297dccd6
Alazar-dev <mealazartadesse@gmail.com>
2021-08-24 11:14:02 +0300

    added form validation

--- src/components/auth/NameAndPostCode.js
+++ src/components/auth/NameAndPostCode.js
@@ -1,32 +1,50 @@
-import { Link } from "react-router-dom";
 import SecurityImage from "../../assets/img/light/Charco Security.png";
 import "./nameAuth.css";
+import { useForm } from "react-hook-form";
 
 export default function AuthComponent() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div className="root-name flex justify-center">
+    <div className="root-name flex align-center mt-24 justify-center">
       <div className="main-name flex flex-col align-center justify-around">
         <p className="greeting-name text-center">
           We need a couple of details to get you setup
         </p>
         <div>
-          <form className="auth-form-name flex flex-col justify-center items-center">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="auth-form-name flex flex-col justify-center items-center"
+          >
             <input
               className="input outline-none"
               type="text"
               placeholder="Name"
+              {...register("name", { required: true })}
             />
+            {errors.name && (
+              <span className="text-red-500">Name field is required</span>
+            )}
             <input
               className="input outline-none"
               type="text"
               placeholder="Post Code"
+              {...register("postCode", { required: true })}
             />
+            {errors.postCode && (
+              <span className="text-red-500">Post Code field is required</span>
+            )}
+            <div className="flex justify-center mt-28">
+              <button className="btn-name shadow-2xl">
+                <p className="btnText-name">Next</p>
+              </button>
+            </div>
           </form>
-        </div>
-        <div className="flex justify-center mb-12">
-          <Link to="/emailAuth" className="btn-name shadow-2xl">
-            <p className="btnText-name">Next</p>
-          </Link>
         </div>
       </div>
       <div>
