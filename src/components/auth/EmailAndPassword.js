914605eac6e0aca30c53eecfa7ec11762afebb62
Alazar-dev <mealazartadesse@gmail.com>
2021-08-25 17:40:34 +0300

    read name from store

--- src/components/auth/EmailAndPassword.js
+++ src/components/auth/EmailAndPassword.js
@@ -1,14 +1,18 @@
+import { useStateMachine } from "little-state-machine";
+import updateAction from "../../actions";
 import EmailAndPasswordForm from "../forms/EmailAndPassword";
 
 import SecurityImage from "../../assets/img/light/Charco Security.png";
 import "./emailAuth.css";
 
 export default function EmailAuthComponent() {
+  const { state } = useStateMachine(updateAction);
+  console.log(state.name);
   return (
     <div className="root-email flex justify-center">
       <div className="main-email flex flex-col align-center justify-around">
         <p className="greeting-email text-center">
-          Hey [Name], Lets create your account
+          Hey {state.name}, Lets create your account
         </p>
         <EmailAndPasswordForm />
       </div>
